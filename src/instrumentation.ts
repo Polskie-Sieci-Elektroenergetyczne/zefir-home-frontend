9e5fa2189dd082b836d324e552cade2343d3bf82
Adam Rolczyk <adam.rolczyk@pse.pl>
2026-08-28 11:08:52 +0200

    feat: add t3 oss env validation

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,11 +1,12 @@
-import * as Sentry from '@sentry/nextjs';
+import { Env } from "@/lib/env";
+import * as Sentry from "@sentry/nextjs";
 
 const sentryOptions: Sentry.NodeOptions | Sentry.EdgeOptions = {
   // Sentry DSN
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn: Env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Enable Spotlight in development
-  spotlight: process.env.NODE_ENV === 'development',
+  spotlight: Env.NODE_ENV === "development",
 
   // Adds request headers and IP for users, for more info visit
   sendDefaultPii: true,
@@ -14,17 +15,17 @@ const sentryOptions: Sentry.NodeOptions | Sentry.EdgeOptions = {
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false
+  debug: false,
 };
 
 export async function register() {
-  if (!process.env.NEXT_PUBLIC_SENTRY_DISABLED) {
-    if (process.env.NEXT_RUNTIME === 'nodejs') {
+  if (!Env.NEXT_PUBLIC_SENTRY_DISABLED) {
+    if (Env.NEXT_RUNTIME === "nodejs") {
       // Node.js Sentry configuration
       Sentry.init(sentryOptions);
     }
 
-    if (process.env.NEXT_RUNTIME === 'edge') {
+    if (Env.NEXT_RUNTIME === "edge") {
       // Edge Sentry configuration
       Sentry.init(sentryOptions);
     }
